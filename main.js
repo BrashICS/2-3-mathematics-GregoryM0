8e734e2a55de51547e8d8f02a88924a4a517c1f2
Gregory Murphy <gregory.murphy1@stu.ocsb.ca>
2024-10-08 11:15:27 -0400

    round

--- main.js
+++ main.js
@@ -19,3 +19,28 @@ console.log(`I generated the number ${random_number}`)
 
 // ***  Your code goes below  ***
 
+
+let celcius = 100;
+let fahrenheit = 5;
+celcius = celcius + 2;
+fahrenheit = (celcius * 9/5) + 32;
+console.log(fahrenheit);
+
+fahrenheit = 77;
+celcius = (fahrenheit - 32) * 5/9;
+console.log (celcius);
+
+console.log (`${celcius} degrees Fahrenheit is ${fahrenheit} degrees celsius.`);
+
+let x1 = -4;
+let x2 = 6;
+let y1 = 5;
+let y2 =0;
+
+m = (y2 - y1)/(x2 - x1);
+console.log (m);
+
+let radius = Math.random () * 10;
+let height = Math.random () * 10;
+let volume = Math.round(Math.PI * radius**2 * height)
+console.log (volume)
